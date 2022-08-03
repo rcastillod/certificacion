62d9bbb30e17c38448ddf9dd0d9b97ddf5c02f86
rcastillod <rodrigo.castillo@global3.cl>
2022-08-03 18:55:17 -0400

    🍀REFACTOR: Desktop and mobile menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,48 +1,88 @@
 <template>
-    <v-app-bar
-        color="white"
-        class="nav-bar"
-        elevation="0"
-        height="80"
-        app
-    >
-        <v-container>
-            <v-row align="center">
-                <v-col cols="6">
-                    <div class="d-flex align-center">
-                        <a
-                            @click="toHome()"
-                        >
-                            <v-img
-                                alt="Vinos Mi Chile"
-                                class="shrink mr-2"
-                                contain
-                                src="../assets/images/logo.svg"
-                                width="250"
-                            />
-                        </a>
-                    </div>
+    <div>
 
-                </v-col>
-                <v-col cols="6" class="d-flex justify-end align-center">
-                    <div class="nav-list mr-4">
-                        <router-link to="/vinos">
-                            <v-btn text>
-                                Vinos
+        <v-app-bar
+            color="white"
+            class="nav-bar"
+            elevation="0"
+            height="80"
+            app
+        >
+            <v-container>
+                <v-row align="center">
+                    <v-col cols="6">
+                        <div class="d-flex align-center">
+                            <a
+                                @click="toHome()"
+                            >
+                                <v-img
+                                    alt="Vinos Mi Chile"
+                                    class="shrink mr-2"
+                                    contain
+                                    src="../assets/images/logo.svg"
+                                    width="250"
+                                />
+                            </a>
+                        </div>
+    
+                    </v-col>
+                    <v-col cols="6" class="d-flex justify-end align-center">
+                        <div class="nav-list mr-4 d-none d-md-block">
+                            <v-btn
+                                v-for="route in routes"
+                                :key="route.title"
+                                @click="redirectTo(route.name)"
+                                :disabled="currentRoute==route.name"
+                                text
+                            >
+                                {{ route.title }}
                             </v-btn>
-                        </router-link>
-                        <v-btn text>
-                            Vinos en Promoción
-                        </v-btn>
-                        <v-btn text>
-                            Accesorios
-                        </v-btn>
-                    </div>
-                    <mini-cart></mini-cart>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-app-bar>
+                            <!-- <router-link to="/vinos">
+                                <v-btn text>
+                                    Vinos
+                                </v-btn>
+                            </router-link>
+                            <v-btn text>
+                                Vinos en Promoción
+                            </v-btn>
+                            <v-btn text>
+                                Accesorios
+                            </v-btn> -->
+                        </div>
+                        <mini-cart></mini-cart>
+                        <v-app-bar-nav-icon 
+                            @click.stop="drawer = !drawer"
+                            class="d-block d-md-none"
+                        ></v-app-bar-nav-icon>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-app-bar>
+        <v-navigation-drawer 
+            v-model="drawer"
+            color="#F6FAFF"
+            fixed 
+            right 
+            temporary
+        >
+            <v-list
+                dense
+                nav
+            >
+                <v-list-item
+                    v-for="route in routes"
+                    :key="route.title"
+                    @click="redirectTo(route.name)"
+                    :disabled="currentRoute==route.name"
+                    link
+                >
+                    <v-list-item-content>
+                        <v-list-item-title>{{ route.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
@@ -51,14 +91,36 @@ import MiniCart from '@/components/MiniCart.vue'
 export default {
     name: 'nav-bar',
     data: function(){
-        return {}
+        return {
+            drawer: false,
+            group: null,
+            routes: [
+                { title: 'Home', name: 'home' },
+                { title: 'Vinos', name: 'vinos' },
+                { title: 'Vinos en Promoción', name: 'promocion' },
+                { title: 'Accesorios', name: 'accesorios' },
+            ],
+        }
+    },
+    computed: {
+        currentRoute(){
+            return this.$route.name
+        }
     },
     methods: {
         toHome() {
             if ( this.$route.name !== 'home' ) {
                 this.$router.push('/')
             }
-        }
+        },
+        redirectTo(nameRoute){
+            this.$router.push({name:nameRoute})
+        },
+    },
+    watch: {
+        group () {
+            this.drawer = false
+        },
     },
     components: {
         'mini-cart': MiniCart
@@ -67,7 +129,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.nav-bar {
-    border-bottom: 1px solid #000;
+.nav-list {
+    a {
+        text-decoration: none;
+    }
 }
 </style>
