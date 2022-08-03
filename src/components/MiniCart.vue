c1bf462f51cdaaed4e178f8a37a8741e4e7c3b99
rcastillod <rodrigo.castillo@global3.cl>
2022-08-02 20:07:14 -0400

    ➕ADD: Site header layout

---
+++ src/components/MiniCart.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-badge
+        :content="messages"
+        :value="messages"
+        color="red"
+        overlap
+    >
+        <v-icon color="black">
+            mdi-cart
+        </v-icon>
+    </v-badge>
+</template>
+
+<script>
+export default {
+    name: 'component-name',
+    // props: {},
+    data: function(){
+        return {
+            messages: 0
+        }
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-app-bar
+        app
+        color="white"
+        class="nav-bar py-2"
+        elevation="0"
+    >
+        <v-container>
+            <v-row align="center">
+                <v-col cols="6">
+                    <div class="d-flex align-center">
+                        <v-img
+                            alt="Vinos Mi Chile"
+                            class="shrink mr-2"
+                            contain
+                            src="../assets/images/logo.svg"
+                            width="250"
+                        />
+                    </div>
+
+                </v-col>
+                <v-col cols="6" class="d-flex justify-end align-center">
+                    <div class="nav-list mr-4">
+                        <v-btn text>
+                            Vinos
+                        </v-btn>
+                        <v-btn text>
+                            Vinos en Promoción
+                        </v-btn>
+                        <v-btn text>
+                            Accesorios
+                        </v-btn>
+                    </div>
+                    <mini-cart></mini-cart>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app-bar>
+</template>
+
+<script>
+import MiniCart from '@/components/MiniCart.vue'
+
+export default {
+    name: 'nav-bar',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    components: {
+        'mini-cart': MiniCart
+    },
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped lang="scss">
+.nav-bar {
+    border-bottom: 1px solid #000;
+}
+</style>
